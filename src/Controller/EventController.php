b06b13ae689ce752d0bbfada5a974cc68a9ba84f
Your Name <you@example.com>
2019-03-26 14:06:54 +0100

    Correction : doctrine query

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -5,6 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use App\Service\EventService;
+use App\Entity\Event;
 
 class EventController extends AbstractController
 {
